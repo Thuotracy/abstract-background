417bdefa6074e983df668cb4f829cec73bb220aa
Thuotracy <tracyjacobs775@gmail.com>
2022-09-21 15:22:22 +0300

    made changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.container');
-for (var i = 1; i<=100; i++){
+for (var i = 1; i<=10; i++){
     const blocks = document.createElement('div')
     blocks.classList.add('block');
     container.appendChild(blocks);
